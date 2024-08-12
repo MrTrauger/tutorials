5175c97d43e13650ae62ba0dfa8985bf2d57a512
MrTrauger <149713045+MrTrauger@users.noreply.github.com>
2024-08-12 11:24:23 -1000

    Create DocumentationAndSystem.java

---
+++ Unit1/DocumentationAndSystem.java
@@ -0,0 +1,23 @@
+/*
+  First tip: when submitting work to GitHub, make sure to end your file name with .java
+  
+  By the way, this is a "block comment!" It starts with "/*" and ends with  the same thing but backwards. 
+  These kinds of comments let you create comments for you
+  to dump a ton of information in without having to squeeze it all on one line. It is useful for
+  explaining what a whole program does altogether or for explaining your own thought process. 
+  I will mainly use it to provide instructions or background information when giving you snippets of code. 
+  They pretty much always go at the start of your code. 
+*/
+
+//this is a line comment and it starts with two slashes. They are for quick snippets of info describing a very small part of your code
+
+/**
+  This is a javadoc comment. It is like a block comment but starts with two asterisks instead of one. 
+  These exist to detail exactly what the next structure is supposed to do. When you comment like this, 
+  your IDE will actually bring up your description when you hover over it even if that code is somewhere in a different file,
+  in case you forget what it does. 
+*/
+
+//TASK 1: Print out "Hello World"
+
+//TASK 2: Print out "Hellow World" and then print it again on the next line
